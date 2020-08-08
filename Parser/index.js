fb4cdc7f52ccba066a7f6c0170b597d597aed307
Brian Liu <brianliu1337@gmail.com>
2020-08-07 22:47:56 -0500

    nick trying stuff after laptop died

--- Parser/index.js
+++ Parser/index.js
@@ -1,17 +1,24 @@
 
 
-function getSourceAsDOM(url) {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.addEventListener("load", function() {
-        let parser = new DOMParser();
-        let URLDOM = parser.parseFromString(this.responseText,"text/html")
-        console.log(URLDOM)
-        let globe = URLDOM.getElementsByClassName('ss-globe')
-        console.log(globe)
-        console.log(globe.getBoundingClientRect())
-    });
-    xmlhttp.open("GET", 'https://cors-anywhere.herokuapp.com/'+url);
-    xmlhttp.send();   
+// function getSourceAsDOM(url) {
+//     let xmlhttp = new XMLHttpRequest();
+//     xmlhttp.addEventListener("load", function() {
+//         let parser = new DOMParser();
+//         let URLDOM = parser.parseFromString(this.responseText,"text/html")
+//         console.log(URLDOM)
+//         let globe = URLDOM.getElementsByClassName('ss-globe')
+//         console.log(globe)
+//         console.log(globe.getBoundingClientRect())
+//     });
+//     xmlhttp.open("GET", 'https://cors-anywhere.herokuapp.com/'+url);
+//     xmlhttp.send();   
+// }
+function getSourceAsDOM(url){
+    let newTab = window.open(url,"_blank")
+    newTab.window.onload = function() {
+        test(this.document)
+        test(this.document.getElementsByClassName('ss-globe'))
+    }
 }
 
 window.onload = function() {
@@ -19,12 +26,3 @@ window.onload = function() {
         getSourceAsDOM("https://post.devpost.com/") 
     })
 }
-
-setup 
-send
-wait
-process
-
-setup
-establish - callback
-send
